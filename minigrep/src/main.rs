ba76a88fc6b5f0f5335dbe88503a08d3ad0b6937
KepKep <eskepkep@gmail.com>
2022-06-18 12:37:51 +0300

    Finished Example
    Finished example from rustdocs "the book"

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,9 +4,7 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
@@ -17,3 +15,8 @@ fn main() {
         process::exit(1);
     }
 }
+
+
+// to enable/disable the "Environment Variables"
+// $Env:CASE_INSENSITIVE=1
+// Remove-Item Env:CASE_INSENSITIVE
